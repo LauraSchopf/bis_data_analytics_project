416bd86c0cd0e409cd39b1feb2901aaf0c7673fc
LauraSchopf <laura.schopf@student.uibk.ac.at>
2025-12-16 13:28:42 +0100

    Initial commit

---
+++ bis_data_analytics_project.R
@@ -0,0 +1,3 @@
+setwd("C:/Users/Laura Schopf/Meine Ablage/Universität Innsbruck/Studium/MSc Wirtschaftsinformatik/01_WS2025-2026/05 Business Information Systems/Part 02 Individual and group work/Clustering group work")
+
+traveldata <- read.csv("travel_platform.csv")
